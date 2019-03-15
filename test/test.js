2d79ce1e8ae9c43527c8ae4d81470d5f79394f01
saifulaiub123 <saifulprogrammer@gmail.com>
2019-03-15 19:00:05 +0600

    5

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var request = require('request');
 
 it('Main page content', function(done) {
     request('http://localhost:3000' , function(error, response, body) {
-        expect(body).to.equal('Hello World');
+        expect(body).to.equal('Hello Worl');
         done();
     });
 });

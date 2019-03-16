e3165a71f257c0f20be6349e6346aaf09c797408
saifulaiub123 <saifulprogrammer@gmail.com>
2019-03-16 16:15:57 +0600

    changed port

--- index.js
+++ index.js
@@ -10,9 +10,19 @@ app.get('/', function (req, res) {
 })
 
 //Launch listening server on port 8080
-app.listen(3000, function () {
-  console.log('App listening on port 3000!')
-})
+//app.listen(3000, function () {
+  //console.log('App listening on port 3000!')
+//})
+
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
+
+
 
 
 module.exports = app;

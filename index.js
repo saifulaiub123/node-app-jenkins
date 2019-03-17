ff29ea584adb5dadf99e19bf5e81a1e650b181aa
saifulaiub123 <saifulprogrammer@gmail.com>
2019-03-17 21:19:34 +0600

    qwqwq

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ var app = express()
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hi! Hello World')
 })
 
-//Launch listening server on port 8080
-//app.listen(3000, function () {
-  //console.log('App listening on port 3000!')
-//})
+Launch listening server on port 8080
+app.listen(3000, function () {
+  console.log('App listening on port 3000!')
+})
 
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-});
+//server.listen(server_port, server_ip_address, function () {
+ // console.log( "Listening on " + server_ip_address + ", port " + server_port )
+//});
   
 
 
